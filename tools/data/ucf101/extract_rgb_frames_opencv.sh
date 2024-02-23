15a0605dc41e0311e57af824fa1edbe381cac599
lianghao02 <lianghao02@megvii.com>
2024-02-23 09:44:50 +0800

    add usage note

--- tools/data/ucf101/extract_rgb_frames_opencv.sh
+++ tools/data/ucf101/extract_rgb_frames_opencv.sh
@@ -5,3 +5,6 @@ python build_rawframes.py ../../data/ucf101/videos/ ../../data/ucf101/rawframes/
 echo "Genearte raw frames (RGB only)"
 
 cd ucf101/
+
+
+python build_rawframes.py /data/dataset/ucf101/videos/ /data/dataset/ucf101/rawframes/ --task rgb --level 2 --ext avi --new-width 320 --new-height 240 --use-opencv
--- tools/data/ucf101/generate_rawframes_filelist.sh
+++ tools/data/ucf101/generate_rawframes_filelist.sh
@@ -6,3 +6,8 @@ PYTHONPATH=. python tools/data/build_file_list.py ucf101 data/ucf101/rawframes/ 
 echo "Filelist for rawframes generated."
 
 cd tools/data/ucf101/
+
+
+
+PYTHONPATH=. python tools/data/build_file_list.py ucf101 /data/dataset/ucf101/rawframes --level 2 --format rawframes --shuffle
+echo "Filelist for rawframes generated."
