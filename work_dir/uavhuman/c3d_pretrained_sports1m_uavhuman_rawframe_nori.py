b4e7deb008dcd6064d710fec02f6e57b54bb10e2
lianghao02 <lianghao02@megvii.com>
2024-03-24 16:44:38 +0800

    update: add uavuman rawframe.file

---
+++ work_dir/uavhuman/c3d_pretrained_sports1m_uavhuman_rawframe_nori.py
@@ -0,0 +1,138 @@
+# -*- coding: utf-8 -*-
+# @Time : 2024/03/24 16:13
+# @Author : Liang Hao
+# @FileName : c3d_pretrained_sports1m_uavhuman_rawframe_nori.py
+# @Email : lianghao@whu.edu.cn
+
+_base_ = [
+    './model/c3d_model.py',
+    '../default_runtime.py'
+]
+
+# dataset settings
+dataset_type = 'RawframeDataset'
+data_root = '/data/dataset/uavhuman/rawframes'
+data_root_val = '/data/dataset/uavhuman/rawframes'
+split = 2  # official train/test splits. valid numbers: 1, 2, 3
+ann_file_train = f'data/uavhuman/uavhuman_train_split_{split}_rawframes.txt'
+ann_file_val = f'data/uavhuman/uavhuman_val_split_{split}_rawframes.txt'
+ann_file_test = f'data/uavhuman/uavhuman_val_split_{split}_rawframes.txt'
+
+file_client_args_train = dict(
+    io_backend='disk',
+    nori_file = 'data/uavhuman/uavhuman_train_split_1_nid.json',
+    dtype = 'uint8',
+    retry = 60
+)
+
+file_client_args_eval = dict(
+    io_backend='disk',
+    nori_file = 'data/uavhuman/uavhuman_val_split_1_nid.json',
+    dtype = 'uint8',
+    retry = 60
+)
+
+train_pipeline = [
+    # dict(type='DecordInit', **file_client_args),
+    dict(type='SampleFrames', clip_len=16, frame_interval=1, num_clips=1),
+    # dict(type='DecordDecode'),
+    dict(type='RawFrameDecodeNoir2', **file_client_args_train),
+    dict(type='Resize', scale=(540, 960)),
+    dict(type='CenterCrop', crop_size=112),
+    dict(type='Flip', flip_ratio=0.5),
+    dict(type='FormatShape', input_format='NCTHW'),
+    dict(type='PackActionInputs')
+]
+val_pipeline = [
+    # dict(type='DecordInit', **file_client_args),
+    dict(
+        type='SampleFrames',
+        clip_len=16,
+        frame_interval=1,
+        num_clips=1,
+        test_mode=True),
+    # dict(type='DecordDecode'),
+    dict(type='RawFrameDecodeNoir2', **file_client_args_eval),
+    dict(type='Resize', scale=(540, 960)),
+    dict(type='CenterCrop', crop_size=112),
+    dict(type='FormatShape', input_format='NCTHW'),
+    dict(type='PackActionInputs')
+]
+test_pipeline = [
+    # dict(type='DecordInit', **file_client_args),
+    dict(
+        type='SampleFrames',
+        clip_len=16,
+        frame_interval=1,
+        num_clips=10,
+        test_mode=True),
+    # dict(type='DecordDecode'),
+    dict(type='RawFrameDecodeNoir2', **file_client_args_eval),
+    dict(type='Resize', scale=(540, 960)),
+    dict(type='CenterCrop', crop_size=112),
+    dict(type='FormatShape', input_format='NCTHW'),
+    dict(type='PackActionInputs')
+]
+
+train_dataloader = dict(
+    batch_size=30,
+    num_workers=8,
+    persistent_workers=False,
+    sampler=dict(type='DefaultSampler', shuffle=True),
+    dataset=dict(
+        type=dataset_type,
+        ann_file=ann_file_train,
+        data_prefix=dict(img=data_root),
+        pipeline=train_pipeline))
+val_dataloader = dict(
+    batch_size=30,
+    num_workers=8,
+    persistent_workers=True,
+    sampler=dict(type='DefaultSampler', shuffle=False),
+    dataset=dict(
+        type=dataset_type,
+        ann_file=ann_file_val,
+        data_prefix=dict(img=data_root_val),
+        pipeline=val_pipeline,
+        test_mode=True))
+test_dataloader = dict(
+    batch_size=1,
+    num_workers=8,
+    persistent_workers=True,
+    sampler=dict(type='DefaultSampler', shuffle=False),
+    dataset=dict(
+        type=dataset_type,
+        ann_file=ann_file_test,
+        data_prefix=dict(img=data_root_val),
+        pipeline=test_pipeline,
+        test_mode=True))
+
+val_evaluator = dict(type='AccMetric')
+test_evaluator = val_evaluator
+
+train_cfg = dict(
+    type='EpochBasedTrainLoop', max_epochs=120, val_begin=1, val_interval=5)
+val_cfg = dict(type='ValLoop')
+test_cfg = dict(type='TestLoop')
+
+param_scheduler = [
+    dict(
+        type='MultiStepLR',
+        begin=0,
+        end=45,
+        by_epoch=True,
+        milestones=[20, 40, 60, 80],
+        gamma=0.1)
+]
+
+optim_wrapper = dict(
+    optimizer=dict(type='SGD', lr=0.001, momentum=0.9, weight_decay=0.0005),
+    clip_grad=dict(max_norm=40, norm_type=2))
+
+default_hooks = dict(checkpoint=dict(interval=5))
+
+# Default setting for scaling LR automatically
+#   - `enable` means enable scaling LR automatically
+#       or not by default.
+#   - `base_batch_size` = (8 GPUs) x (30 samples per GPU).
+auto_scale_lr = dict(enable=False, base_batch_size=240)
